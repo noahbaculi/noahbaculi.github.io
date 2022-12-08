59681f636c5b07ed77962e4a5d449215622e9860
Noah <nbaculi@hotmail.com>
2022-12-07 16:47:30 -0800

    fix generateTabString

--- assets/js/guitartab.js
+++ assets/js/guitartab.js
@@ -8,38 +8,6 @@ function print(...objs) {
 	}
 }
 
-const pitchInput = document.getElementById("pitchInput");
-
-// Add event listener for the pitch input text area to deactivate TAB output
-pitchInput.addEventListener("input", () => {
-	deactivateTabOutput();
-});
-
-// Add event listeners to generate TAB output after changes
-pitchInput.addEventListener("change", () => {
-	generateTab();
-});
-for (const settingSelectId of ["guitarTuning", "guitarCapo"]) {
-	document.getElementById(settingSelectId).addEventListener("change", () => {
-		generateTab();
-	});
-}
-
-// Add event listeners to reformat TAB string output after changes
-for (const displaySettingId of ["tabLineLength", "numBeatSeparators"]) {
-	document.getElementById(displaySettingId).addEventListener("input", () => {
-		updateLineLengthLabel();
-		if (tabData !== undefined) {
-			displayTab(tabData);
-		}
-	});
-}
-
-function deactivateTabOutput() {
-	const tabOutput = document.getElementById("tabOutput");
-	tabOutput.disabled = true;
-}
-
 // Maintain latest tabData so formatting settings can update output without
 // recomputing tabData
 let tabData;
@@ -85,42 +53,6 @@ function updateLineLengthLabel() {
 	document.getElementById("tabLineLengthLabel").innerHTML = `Line Length - ${tabLineLength}`;
 }
 
-function displayTab(tabData) {
-	let tabLineLength = 80;
-	try {
-		tabLineLength = parseInt(document.getElementById("tabLineLength").value);
-	} catch (error) {
-		console.warn(`Something went wrong... Using default tabLineLength = ${tabLineLength} | Error = ${error}`);
-	}
-
-	let numBeatSeparators = 1;
-	try {
-		numBeatSeparators = parseInt(document.getElementById("numBeatSeparators").value);
-	} catch (error) {
-		console.warn(`Something went wrong... Using default numBeatSeparators = ${numBeatSeparators} | Error = ${error}`);
-	}
-
-	const tabString = generateTabString(tabData, tabLineLength, numBeatSeparators);
-	tabOutput.value = tabString;
-	tabOutput.disabled = false;
-}
-
-const testNotesString = `E4
-Eb4
-E4
-Eb4
-E4
-B3
-D4
-C4
-
-A2A3
-E3
-A3
-C3
-E3
-A3`;
-
 /**
  * Format TAB data to plain text string
  */
@@ -138,24 +70,47 @@ function generateTabString(tabData, lineLength = 80, numBeatSeparators = 1) {
 			[6, []],
 		]);
 
+		const beatStrings = new Set(stringOutputs.keys());
 		for (const beatData of tabData) {
-			let beatStrings = new Set(stringOutputs.keys());
 			if (beatData === "break") {
 				for (const stringNum of beatStrings) {
 					stringOutputs.get(stringNum).push("|");
 				}
 			} else {
 				// Add the fret numbers for each of the fingerings for the beat
-				for (const stringDatum of beatData) {
-					const stringNum = stringDatum["stringNum"];
-					stringOutputs.get(stringNum).push(stringDatum["fret"]);
-					beatStrings.delete(stringNum);
-				}
 
-				// Add dashes for remaining guitar strings in the beat
-				for (const remainingStringNum of beatStrings) {
-					stringOutputs.get(remainingStringNum).push("-");
-				}
+				const maxFingeringWidth = beatData.reduce(
+					(max, stringDatum) => Math.max(max, stringDatum["fret"].toString().length),
+					0
+				);
+
+				const beatStringToFret = beatData.reduce(
+					(map, stringDatum) => map.set(stringDatum["stringNum"], stringDatum["fret"].toString()),
+					new Map([
+						[1, null],
+						[2, null],
+						[3, null],
+						[4, null],
+						[5, null],
+						[6, null],
+					])
+				);
+
+				beatStringToFret.forEach((fret, stringNum) => {
+					let fretOutput = "";
+					if (fret === null) {
+						// If no fret for that string
+						fretOutput = "-".repeat(maxFingeringWidth);
+					} else {
+						fretOutput += fret;
+
+						// Add additional dashes if there is a wider fingering
+						// in the same beat
+						const currFretWidthDelta = maxFingeringWidth - fret.length;
+						fretOutput += "-".repeat(currFretWidthDelta);
+					}
+					stringOutputs.get(stringNum).push(fretOutput);
+				});
 			}
 		}
 		return stringOutputs;
@@ -186,5 +141,74 @@ function generateTabString(tabData, lineLength = 80, numBeatSeparators = 1) {
 
 	return outputString;
 }
+
+function displayTab(tabData) {
+	let tabLineLength = 80;
+	try {
+		tabLineLength = parseInt(document.getElementById("tabLineLength").value);
+	} catch (error) {
+		console.warn(`Something went wrong... Using default tabLineLength = ${tabLineLength} | Error = ${error}`);
+	}
+
+	let numBeatSeparators = 1;
+	try {
+		numBeatSeparators = parseInt(document.getElementById("numBeatSeparators").value);
+	} catch (error) {
+		console.warn(`Something went wrong... Using default numBeatSeparators = ${numBeatSeparators} | Error = ${error}`);
+	}
+
+	const tabString = generateTabString(tabData, tabLineLength, numBeatSeparators);
+	tabOutput.value = tabString;
+	tabOutput.disabled = false;
+}
+
+function deactivateTabOutput() {
+	const tabOutput = document.getElementById("tabOutput");
+	tabOutput.disabled = true;
+}
+
+const pitchInput = document.getElementById("pitchInput");
+// Add event listener for the pitch input text area to deactivate TAB output
+pitchInput.addEventListener("input", () => {
+	deactivateTabOutput();
+});
+
+// Add event listeners to generate TAB output after changes
+pitchInput.addEventListener("change", () => {
+	generateTab();
+});
+for (const settingSelectId of ["guitarTuning", "guitarCapo"]) {
+	document.getElementById(settingSelectId).addEventListener("change", () => {
+		generateTab();
+	});
+}
+
+// Add event listeners to reformat TAB string output after changes
+for (const displaySettingId of ["tabLineLength", "numBeatSeparators"]) {
+	document.getElementById(displaySettingId).addEventListener("input", () => {
+		updateLineLengthLabel();
+		if (tabData !== undefined) {
+			displayTab(tabData);
+		}
+	});
+}
+
+document.getElementById("generateTabButton").addEventListener("click", generateTab);
+
+const testNotesString = `E4
+Eb4
+E4
+Eb4
+E4
+B3
+D4
+C4
+
+A2A3
+E3
+A3
+C3
+E3
+A3`;
 
 updateLineLengthLabel();

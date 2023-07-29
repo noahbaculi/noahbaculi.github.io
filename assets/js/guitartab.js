38e0213089be3a2176d9a68d2285fe13c0daced2
Noah <nbaculi@hotmail.com>
2023-07-29 00:46:19 -0700

    working guitar tab generator

--- assets/js/guitartab.js
+++ assets/js/guitartab.js
@@ -8,6 +8,15 @@ function print(...objs) {
 	}
 }
 
+import init, {
+	wasm_create_guitar_compositions,
+	get_tuning_names,
+} from "../wasm_guitar_tab_generator/guitar_tab_generator.js";
+async function run() {
+	await init();
+}
+run();
+
 // Maintain latest guitar so formatting settings can update output without
 // recomputing tabData
 let guitar;
@@ -31,53 +40,39 @@ function createGuitar() {
 }
 createGuitar(); // create default standard guitar
 
-// Maintain latest tabData so formatting settings can update output without
-// recomputing tabData
-let tabData;
-function createArrangement() {
-	const pitchInput = document.getElementById("pitchInput");
-
-	// Reset playback
-	playbackBeatNumber = -1;
-	stopPlayback();
-
-	try {
-		const arrangement = new exports.Arrangement(guitar, pitchInput.value);
-		tabData = arrangement.bestFingerings;
-	} catch (error) {
-		console.error(error);
-		document.getElementById("tabOutput").value = error;
-		document.getElementById("tabOutput").disabled = true;
-		document.getElementById("playbackMenu").hidden = true;
-		return;
-	}
-	displayTab(tabData);
-	document.getElementById("playbackMenu").hidden = false;
-}
-
-function playBeatAudio() {
+function playBeatAudio(pitches) {
 	// Iterate beat number
-	playbackBeatNumber++;
-	const beatData = tabData[playbackBeatNumber];
+	if (playbackBeatNumber === null) {
+		playbackBeatNumber = 0;
+	} else {
+		playbackBeatNumber++;
+	}
 
-	if (playbackBeatNumber >= tabData.length) {
+	const beatPitches = pitches[playbackBeatNumber];
+
+	console.log(beatPitches);
+
+	if (playbackBeatNumber >= pitches.length) {
 		// Done playing
 		stopPlayback();
 		return;
 	}
-	// Skip breaks
-	if (beatData === "break") {
-		playBeatAudio();
+	// Skip measure breaks
+	if (beatPitches[0] == "MEASURE_BREAK") {
+		numMeasureBreaks++;
+		console.log("Skipping playback");
+		playBeatAudio(pitches);
 		return;
 	}
+	if (beatPitches[0] !== "REST") {
+		playbackSynth.triggerAttackRelease(beatPitches, "8n");
+	}
 
-	const beatPitches = beatData.get("pitches");
-	playbackSynth.triggerAttackRelease(beatPitches, "8n");
-
-	displayTab(tabData, playbackBeatNumber);
+	generateTab();
 }
 
-let playbackBeatNumber = -1;
+let playbackBeatNumber = null;
+let numMeasureBreaks = 0;
 let playbackSynth = null;
 let currentlyPlayingAudioFunctionRepeatInterval = null;
 function playTabAudio() {
@@ -90,7 +85,11 @@ function playTabAudio() {
 	playbackSynth ??= new exports.Tone.PolySynth().toDestination();
 	playbackSynth.set({ detune: -1200 });
 
-	currentlyPlayingAudioFunctionRepeatInterval = setInterval(playBeatAudio, 500);
+	console.info("Playing tab audio");
+
+	let pitches = generateTab();
+
+	currentlyPlayingAudioFunctionRepeatInterval = setInterval(playBeatAudio, 500, pitches);
 }
 
 function updateLineLengthLabel() {
@@ -105,124 +104,6 @@ function updateLineLengthLabel() {
 }
 updateLineLengthLabel(); // update line length label with initial default value
 
-function displayTab(tabData, currentlyPlayingIndex = -1) {
-	let tabLineLength = 80;
-	try {
-		tabLineLength = parseInt(document.getElementById("tabLineLength").value);
-	} catch (error) {
-		console.warn(`Something went wrong... Using default tabLineLength = ${tabLineLength} | Error = ${error}`);
-	}
-
-	let numBeatSeparators = 1;
-	try {
-		numBeatSeparators = parseInt(document.getElementById("numBeatSeparators").value);
-	} catch (error) {
-		console.warn(`Something went wrong... Using default numBeatSeparators = ${numBeatSeparators} | Error = ${error}`);
-	}
-
-	/**
-	 * Format TAB data to plain text string
-	 */
-	function generateTabString(tabData, lineLength = 80, numBeatSeparators = 1, currentlyPlayingIndex = -1) {
-		/**
-		 * Create single long bare TAB plain text string
-		 */
-		function generateTabStringData(tabData, currentlyPlayingIndex = -1) {
-			const stringOutputs = new Map([
-				[1, []],
-				[2, []],
-				[3, []],
-				[4, []],
-				[5, []],
-				[6, []],
-				["playbackIndicator", []],
-			]);
-
-			// for (const beatData of tabData) {
-			tabData.forEach((beatData, beatIndex) => {
-				// Add measure breaks
-				if (beatData === "break") {
-					for (const stringNum of [1, 2, 3, 4, 5, 6]) {
-						stringOutputs.get(stringNum).push("|");
-					}
-					stringOutputs.get("playbackIndicator").push(" ");
-					return;
-				}
-
-				// Calculate the max width of the fret fingerings
-				const beatStringToFret = beatData.get("stringToFretFingering");
-				const fretValues = Array.from(beatStringToFret.values()).filter((fretVal) => fretVal !== null);
-				const fretValueLengths = fretValues.map((fretVal) => fretVal.toString().length);
-				const maxFretValueLength = Math.max(...fretValueLengths);
-
-				// Add the fret numbers for each of the fingerings for the beat
-				beatStringToFret.forEach((fret, stringNum) => {
-					let fretOutput = "";
-					if (fret === null) {
-						// If no fret for that string
-						fretOutput = "-".repeat(maxFretValueLength);
-					} else {
-						fretOutput += fret;
-
-						// Add additional dashes if there is a wider fingering
-						// in the same beat
-						const currFretWidthDelta = maxFretValueLength - fretOutput.length;
-
-						fretOutput += "-".repeat(currFretWidthDelta);
-					}
-					stringOutputs.get(stringNum).push(fretOutput);
-				});
-
-				let playbackIndicatorOutput = " ".repeat(maxFretValueLength);
-				if (beatIndex === currentlyPlayingIndex) {
-					playbackIndicatorOutput = " ".repeat(maxFretValueLength - 1) + "↑";
-				}
-				stringOutputs.get("playbackIndicator").push(playbackIndicatorOutput);
-			});
-			return stringOutputs;
-		}
-		const tabStringData = generateTabStringData(tabData, currentlyPlayingIndex);
-
-		// Add dash separators
-		const beatSeparator = "-".repeat(numBeatSeparators); // to customize spacing
-		const playbackBeatSeparator = " ".repeat(numBeatSeparators); // to customize spacing
-		let tabCombinedStrings = new Map();
-		tabStringData.forEach((value, key) => {
-			if (key === "playbackIndicator") {
-				tabCombinedStrings.set(key, playbackBeatSeparator + value.join(playbackBeatSeparator) + playbackBeatSeparator);
-			} else {
-				tabCombinedStrings.set(key, beatSeparator + value.join(beatSeparator) + beatSeparator);
-			}
-		});
-
-		// Apply line length
-		let outputString = "";
-		while (tabCombinedStrings.get(1).length > 0) {
-			for (const stringNum of tabCombinedStrings.keys()) {
-				outputString += tabCombinedStrings.get(stringNum).substring(0, lineLength);
-
-				// Remove handled beats
-				tabCombinedStrings.set(stringNum, tabCombinedStrings.get(stringNum).slice(lineLength));
-				outputString += "\n";
-
-				if (stringNum === "playbackIndicator") {
-					outputString += "\n\n";
-				}
-			}
-		}
-
-		return outputString;
-	}
-
-	const tabString = generateTabString(tabData, tabLineLength, numBeatSeparators, currentlyPlayingIndex);
-	document.getElementById("tabOutput").value = tabString;
-	document.getElementById("tabOutput").disabled = false;
-}
-
-function deactivateTabOutput() {
-	document.getElementById("tabOutput").disabled = true;
-}
-
 function loadExampleSong() {
 	const exSongs = {
 		"Fur Elise": `E4
@@ -233,26 +114,26 @@ function loadExampleSong() {
 					B3
 					D4
 					C4
-
+					---
 					A2A3
 					E3
 					A3
 					C3
 					E3
 					A3
-
+					---
 					E3B3
 					E3
 					Ab3
 					E3
 					Ab3
 					B3
-
+					---
 					A2C4
 					E3
 					A3
 					E3
-
+					---
 					E4
 					Eb4
 					E4
@@ -261,14 +142,14 @@ function loadExampleSong() {
 					B3
 					D4
 					C4
-
+					---
 					A2A3
 					E3
 					A3
 					C3
 					E3
 					A3
-
+					---
 					E3B3
 					E3
 					Ab3
@@ -276,7 +157,7 @@ function loadExampleSong() {
 					C4
 					B3
 					A3
-
+					---
 					C4
 					C4
 					C4
@@ -285,7 +166,7 @@ function loadExampleSong() {
 					E4
 					E4
 					D4
-
+					---
 					Bb4
 					A4
 					A4
@@ -294,7 +175,7 @@ function loadExampleSong() {
 					E4
 					D4
 					C4
-
+					---
 					Bb3
 					Bb3
 					A3
@@ -302,7 +183,7 @@ function loadExampleSong() {
 					A3
 					Bb3
 					C4
-
+					---
 					D4
 					Eb4
 					Eb4
@@ -310,7 +191,7 @@ function loadExampleSong() {
 					F4
 					A3
 					C4
-
+					---
 					D4
 					B3
 					C4`,
@@ -326,7 +207,7 @@ function loadExampleSong() {
 						A3
 						B3
 						A2C4
-
+						---
 						A3
 						A3
 						G#3
@@ -334,7 +215,7 @@ function loadExampleSong() {
 						B3
 						G#3
 						E3
-
+						---
 						A3
 						A2C4
 						A3D4
@@ -348,7 +229,7 @@ function loadExampleSong() {
 						B3
 						A2C4
 						B3
-
+						---
 						A3
 						A2G#3
 						F#3
@@ -360,37 +241,37 @@ function loadExampleSong() {
 						A3
 						G3
 						C3E3
-
+						---
 						C3G3
 						A3
 						G3
 						C3E3
-
+						---
 						G2D4
 						D4
 						B3
-
+						---
 						C3C4
 						C4
 						E3G3
-
+						---
 						F3A3
 						A3
 						C4
 						B3
 						A3
-
+						---
 						C3G3
 						A3
 						G3
 						C3E3
-
+						---
 						C3A3
 						A3
 						C4
 						B3
 						A3
-
+						---
 						C3G3
 						A3
 						C3G3
@@ -398,21 +279,21 @@ function loadExampleSong() {
 						G3
 						C3D3
 						E3
-
+						---
 						G2D4
 						D4
 						F4
 						D4
 						B3
-
+						---
 						C3C4
 						E2A2E3A3C4E4
-
+						---
 						C3C4
 						G3
 						E3
 						G3
-
+						---
 						F3
 						D3
 						C3`,
@@ -420,52 +301,52 @@ function loadExampleSong() {
 							G3
 							D4
 							D4
-
+							---
 							E4
 							E4
 							D4
-
+							---
 							C4
 							C4
 							B3
 							B3
-
+							---
 							A3
 							A3
 							G3
-
+							---
 							D4
 							D4
 							C4
 							C4
-
+							---
 							B3
 							B3
 							A3
-
+							---
 							D4
 							D4
 							C4
 							C4
-
+							---
 							B3
 							B3
 							A3
-
+							---
 							G3
 							G3
 							D4
 							D4
-
+							---
 							E4
 							E4
 							D4
-
+							---
 							C4
 							C4
 							B3
 							B3
-
+							---
 							A3
 							A3
 							G3`,
@@ -475,7 +356,7 @@ function loadExampleSong() {
 					C4
 					D4
 					G3
-
+					---
 					G3
 					A3
 					Bb3
@@ -487,7 +368,7 @@ function loadExampleSong() {
 					C4
 					Bb3
 					A3
-
+					---
 					C4
 					D4
 					D4
@@ -499,7 +380,7 @@ function loadExampleSong() {
 					F4
 					D4
 					C4
-
+					---
 					F3
 					G3
 					A3
@@ -510,15 +391,15 @@ function loadExampleSong() {
 					A3
 					E3
 					F3
-
-
+					---
+					---
 					C4
 					A3
 					A3
 					C4
 					D4
 					G3
-
+					---
 					G3
 					A3
 					Bb3
@@ -530,7 +411,7 @@ function loadExampleSong() {
 					C4
 					Bb3
 					A3
-
+					---
 					C4
 					D4
 					D4
@@ -542,7 +423,7 @@ function loadExampleSong() {
 					F4
 					D4
 					C4
-
+					---
 					F3
 					G3
 					A3
@@ -553,8 +434,8 @@ function loadExampleSong() {
 					A3
 					E3
 					F3
-
-
+					---
+					---
 					F3
 					F4
 					D4
@@ -563,12 +444,12 @@ function loadExampleSong() {
 					C4
 					Bb3
 					D4
-
+					---
 					F4
 					D4
 					F4
 					Bb3
-
+					---
 					F4
 					D4
 					C4
@@ -588,7 +469,7 @@ function loadExampleSong() {
 	const exSongNotes = exSongs[exSongInputName].replaceAll("\t", "");
 	pitchInput.value = `${pitchInput.value}\n\n// Example\n// ${exSongInputName}\n${exSongNotes}`;
 
-	createArrangement();
+	newTab();
 }
 
 function stopPlayback() {
@@ -605,15 +486,16 @@ function stopPlayback() {
 }
 
 const pitchInput = document.getElementById("pitchInput");
-// Add event listener for the pitch input text area to deactivate TAB output
-pitchInput.addEventListener("input", deactivateTabOutput);
+pitchInput.addEventListener("input", () => {
+	// deactivateTabOutput();
+	newTab();
+});
 
 // Add event listeners to generate TAB output after changes
-pitchInput.addEventListener("change", createArrangement);
+// pitchInput.addEventListener("change", createArrangement);
 for (const settingSelectId of ["guitarTuning", "guitarCapo"]) {
 	document.getElementById(settingSelectId).addEventListener("change", () => {
-		createGuitar();
-		createArrangement();
+		newTab();
 	});
 }
 
@@ -622,17 +504,19 @@ const exampleSongsInput = document.getElementById("exampleSongs");
 exampleSongsInput.addEventListener("change", loadExampleSong);
 
 // Add event listeners to reformat TAB string output after changes
-for (const displaySettingId of ["tabLineLength", "numBeatSeparators"]) {
+for (const displaySettingId of ["tabLineLength", "tabPadding"]) {
 	document.getElementById(displaySettingId).addEventListener("input", () => {
 		updateLineLengthLabel();
-		if (tabData !== undefined) {
-			displayTab(tabData, playbackBeatNumber);
-		}
+		newTab();
 	});
 }
 
 // Add event listener to export tab output when export button is pressed
 document.getElementById("exportButton").addEventListener("click", () => {
+	// playbackSynth ??= new exports.Tone.PolySynth().toDestination();
+	// playbackSynth.triggerAttackRelease(["A2", "A3"], "8n");
+	// console.log("Good");
+
 	// Download tab output
 	const fileContent = document.getElementById("tabOutput").value;
 	const temporaryDownloadElement = document.createElement("a");
@@ -646,25 +530,103 @@ document.getElementById("exportButton").addEventListener("click", () => {
 	alert("Tab output saved to your downloads! 🎉");
 });
 
-// Add event listener to export tab output when export button is pressed
 document.getElementById("playButton").addEventListener("click", () => {
-	if (!(Symbol.iterator in Object(tabData))) {
-		return;
-	}
 	document.getElementById("resetPlaybackButton").disabled = false;
 	document.getElementById("pauseButton").style.display = "flex";
 	document.getElementById("playButton").style.display = "none";
 	playTabAudio();
 });
 
-// Add event listener to export tab output when export button is pressed
 document.getElementById("pauseButton").addEventListener("click", () => {
 	stopPlayback();
 });
 
-// Add event listener to export tab output when export button is pressed
 document.getElementById("resetPlaybackButton").addEventListener("click", () => {
-	playbackBeatNumber = -1;
-	stopPlayback();
-	displayTab(tabData);
+	newTab();
 });
+
+function newTab() {
+	// Reset playback
+	playbackBeatNumber = null;
+	numMeasureBreaks = 0;
+	stopPlayback();
+	generateTab();
+}
+
+function generateTab() {
+	let startTime = performance.now();
+
+	let input = getInput();
+
+	console.log(input);
+	try {
+		let compositions = wasm_create_guitar_compositions(input);
+		document.getElementById("tabOutput").value = compositions[0].tab;
+
+		// console.log(`Tab:\n${tabOutput}`);
+		let endTime = performance.now();
+		let duration = (endTime - startTime).toFixed(1);
+		console.info(`Arrangement generated in ${duration} milliseconds:`, compositions[0]);
+
+		document.getElementById("tabOutput").disabled = false;
+		document.getElementById("playbackMenu").hidden = false;
+
+		let pitches = compositions[0].pitches;
+		return pitches;
+	} catch (error) {
+		console.warn(error);
+		document.getElementById("tabOutput").value = error;
+
+		document.getElementById("tabOutput").disabled = true;
+		document.getElementById("playbackMenu").hidden = true;
+	}
+	return null;
+}
+
+function getInput() {
+	const pitchInput = document.getElementById("pitchInput").value;
+
+	let guitarTuning = "standard";
+	try {
+		guitarTuning = document.getElementById("guitarTuning").value;
+	} catch (error) {
+		console.warn(`Something went wrong... Using default guitarTuning = ${guitarTuning} | Error = ${error}`);
+	}
+
+	let guitarCapo = 0;
+	try {
+		guitarCapo = parseInt(document.getElementById("guitarCapo").value);
+	} catch (error) {
+		console.warn(`Something went wrong... Using default guitarCapo = ${guitarCapo} | Error = ${error}`);
+	}
+
+	let tabLineLength = 80;
+	try {
+		tabLineLength = parseInt(document.getElementById("tabLineLength").value);
+	} catch (error) {
+		console.warn(`Something went wrong... Using default tabLineLength = ${tabLineLength} | Error = ${error}`);
+	}
+
+	let tabPadding = 1;
+	try {
+		tabPadding = parseInt(document.getElementById("tabPadding").value);
+	} catch (error) {
+		console.warn(`Something went wrong... Using default tabPadding = ${tabPadding} | Error = ${error}`);
+	}
+
+	let playback_index = null;
+	if (playbackBeatNumber !== null) {
+		playback_index = playbackBeatNumber - numMeasureBreaks;
+	}
+
+	return {
+		pitches: pitchInput,
+		tuning_name: guitarTuning,
+		guitar_num_frets: 18,
+		guitar_capo: 0,
+		num_arrangements: 1,
+		width: tabLineLength,
+		padding: tabPadding,
+		playback_index: playback_index,
+	};
+}

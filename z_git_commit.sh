f077be7072f982b5ca7e161da335c54452916c55
Noah <nbaculi@hotmail.com>
2024-03-30 14:05:03 -0700

    update commit script

--- z_git_commit.sh
+++ z_git_commit.sh
@@ -1,14 +1,14 @@
-git checkout master
+git checkout staging
 git status
 
-read -p "Git commit all to master? ([y]/n): " continue_input
+read -p "Git commit all to staging? ([y]/n): " continue_input
 
 
 if [[ $continue_input == *"n"* ]]; then
 	echo "Not committing..."
 
 else
-	echo "Committing all to master..."
+	echo "Committing all to staging..."
 	read -p "Git commit message: " commit_msg_input
 	git add .
 	git commit -m "$commit_msg_input"
@@ -21,9 +21,9 @@ read -p "Git merge to production branch? (y/[n]): " merge_production_input
 if [[ $merge_production_input == *"y"* ]]; then
 	echo "Merging to production..."
 	git checkout production
-	git merge master
+	git merge staging
 	git push
-	git checkout master
+	git checkout staging
 
 else
 	echo "Not merging..."

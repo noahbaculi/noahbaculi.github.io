895ce1da36d1da8d594c4810ab0b46f74a533e80
Noah Baculi <noahbaculi@gmail.com>
2026-02-03 08:08:55 -0800

    iter hobbies

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,10 +54,10 @@ async function initNavbars() {
 
     // Optionally load subnavbars
     if (window.location.pathname.includes("/hobbies")) {
-      await loadSection("#subnavbar", "/assets/html/subnavbar_hobbies.html");
+      await loadSection("#subnavbars", "/assets/html/subnavbar_hobbies.html");
     } else if (window.location.pathname.includes("/professional")) {
       await loadSection(
-        "#subnavbar",
+        "#subnavbars",
         "/assets/html/subnavbar_professional.html",
       );
     }

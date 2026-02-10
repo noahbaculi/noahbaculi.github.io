defb3395ad1a41224867375b73a75c6a08495c46
Noah Baculi <noahbaculi@gmail.com>
2026-02-09 16:57:01 -0800

    mv image generator deps to script header

--- images/image_generator.py
+++ images/image_generator.py
@@ -1,3 +1,10 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "pillow",
+# ]
+# ///
+
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from pathlib import Path

353f1995adf2d6389d09053f0689dad02187c9b5
Noah Baculi <noahbaculi@gmail.com>
2026-02-20 14:54:07 -0800

    rm unused breakpoints.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,16 +91,6 @@ async function loadHomepageSections() {
 loadHomepageSections();
 initNavbars();
 
-// Breakpoints (requires breakpoints.min.js)
-breakpoints({
-  xlarge: ["1281px", "1680px"],
-  large: ["981px", "1280px"],
-  medium: ["737px", "980px"],
-  small: ["481px", "736px"],
-  xsmall: ["361px", "480px"],
-  xxsmall: [null, "360px"],
-});
-
 // Play initial animations on page load.
 window.addEventListener("load", function () {
   window.setTimeout(function () {
